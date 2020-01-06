08478797032651a0d51a038ee8b004f6d3f33c95
skimanikings <kimanisimon856@gmail.com>
2020-01-06 13:56:10 +0300

    get the toheadlines of the news using the newsapi

--- app.py
+++ app.py
@@ -11,3 +11,4 @@ def index():
  A view root page function that returns the index page and its data
     """
     newsapi = NewsApiClient(api_key="cca440cda74d458a8d8d34a747f29ac8")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
