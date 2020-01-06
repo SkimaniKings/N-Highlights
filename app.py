df15f65630109b4fdc200a8ca455fc73934dc0e9
skimanikings <kimanisimon856@gmail.com>
2020-01-06 16:39:22 +0300

    add a read more button that is link to the articles

--- app.py
+++ app.py
@@ -14,22 +14,26 @@ def index():
     topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
     
     articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
     
-    desc = []
-    news = []
-    img = []
-
+  
     
     for i in range(len(articles)):
         myarticles = articles[i]
         
         news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
  
         
-    mylist = zip(news, desc, img) 
+    mylist=zip(des,image,news,pubAt,url)
     return render_template('index.htm', context = mylist)
 if __name__ == "__main__":
     app.run(debug=True)
