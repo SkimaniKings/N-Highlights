c43de5d759f68ef8b9838adf8a9325c15f20dc7d
skimanikings <kimanisimon856@gmail.com>
2020-01-06 13:46:34 +0300

    create a function that returns the index page and its data

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask
 from newsapi import NewsApiClient
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    
+  """
+ A view root page function that returns the index page and its data
+  """
