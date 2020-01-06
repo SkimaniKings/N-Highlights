6d4baf77b418fb330ff180b9293ded11e0fbe536
skimanikings <kimanisimon856@gmail.com>
2020-01-06 13:51:06 +0300

    create a new api oject with newsapi client and api key

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     
-  """
+    
+    """
  A view root page function that returns the index page and its data
-  """
+    """
+    newsapi = NewsApiClient(api_key="cca440cda74d458a8d8d34a747f29ac8")
