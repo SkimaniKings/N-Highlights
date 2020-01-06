f0555c3ffb231e6d9f1a630df2af1f827005cfec
skimanikings <kimanisimon856@gmail.com>
2020-01-06 14:33:46 +0300

    generate a newapi key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     """
  A view root page function that returns the index page and its data
     """
-    newsapi = NewsApiClient(api_key="cca440cda74d458a8d8d34a747f29ac8")
+    newsapi = NewsApiClient(api_key="9928e2b4e73e42319e3b5832e0c6c672")
     topheadlines = newsapi.get_top_headlines(sources="abc-news")
     
     articles = topheadlines['articles']
@@ -23,11 +23,15 @@ def index():
     for i in range(len(articles)):
         myarticles = articles[i]
         
-        heading.append(myarticles['tittle'])
+        heading.append(myarticles['title'])
         news.append(myarticles['description'])
         image.append(myarticles['urlToImage'])
         date.append(myarticles['publishedAt'])
         
         mylist = zip(heading,news,image,date)
+        
+        return render_template('index.htm', context = mylist)
+if __name__ == "__main__":
+    app.run(debug=True)
         
  
