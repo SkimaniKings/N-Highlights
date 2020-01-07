fa48fef9c5e8423730c09d5413813d4c83a7bf95
skimanikings <kimanisimon856@gmail.com>
2020-01-07 08:28:35 +0300

    add Aljazeera news source

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def Bbc():
  A view root page function that returns the index page and its data
     """
     newsapi = NewsApiClient(api_key="28d135ff8b27447caf2f3b7cd13fb15a")
-    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
     
     articles = topheadlines['articles']
      
@@ -99,6 +99,71 @@ def Bbc():
         
     mylist=zip(des,image,news,pubAt,url)
     return render_template('Bbc.htm', context = mylist)
+@app.route('/Cnn')
+def Cnn():
+    
+    
+    """
+ A view root page function that returns the index page and its data
+    """
+    newsapi = NewsApiClient(api_key="28d135ff8b27447caf2f3b7cd13fb15a")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    
+    articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
+    
+  
+    
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+ 
+        
+    mylist=zip(des,image,news,pubAt,url)
+    return render_template('Cnn.htm', context = mylist)
+@app.route('/Aljazeera')
+def Aljzeera():
+    
+    
+    """
+ A view root page function that returns the index page and its data
+    """
+    newsapi = NewsApiClient(api_key="28d135ff8b27447caf2f3b7cd13fb15a")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    
+    articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
+    
+  
+    
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+ 
+        
+    mylist=zip(des,image,news,pubAt,url)
+    return render_template('Aljzeera.htm', context = mylist)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
