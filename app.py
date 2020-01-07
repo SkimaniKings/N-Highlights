4b334bfe62238aac064bcb2c62b9450df3b4f0ec
skimanikings <kimanisimon856@gmail.com>
2020-01-07 04:43:01 +0300

    add a tittle to the readme

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
  A view root page function that returns the index page and its data
     """
     newsapi = NewsApiClient(api_key="28d135ff8b27447caf2f3b7cd13fb15a")
-    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
     
     articles = topheadlines['articles']
      
@@ -35,6 +35,39 @@ def index():
         
     mylist=zip(des,image,news,pubAt,url)
     return render_template('index.htm', context = mylist)
+@app.route('/abc')
+def abc():
+    
+    
+    """
+ A view root page function that returns the index page and its data
+    """
+    newsapi = NewsApiClient(api_key="28d135ff8b27447caf2f3b7cd13fb15a")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    
+    articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
+    
+  
+    
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+ 
+        
+    mylist=zip(des,image,news,pubAt,url)
+    return render_template('abc.htm', context = mylist)
+
 if __name__ == "__main__":
     app.run(debug=True)
         
---
+++ readme.md
@@ -0,0 +1,5 @@
+### Table of Contents
+- Project Description 
+
+# News-Highlights
+
